ec5faff6e3f43bbf395c0ceca703ebc1baf05169
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-11-01 14:42:47 +0530

    initialize app with tailwindscss

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-cyan-500 text-2xl text-white font-bold bg-center">
+        <p>Hello from chirp-frontend</p>
+      </div>
+    </>
+  )
+}
+
+export default App
