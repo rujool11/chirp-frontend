c90c5b31917d8d225c77d252b278f6bdcfdb02b2
rujool11 <132924872+rujool11@users.noreply.github.com>
2025-11-03 18:12:09 +0530

    add routing structure and page skeletons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ErrorPage from './pages/ErrorPage'
 
 function App() {
 
   return (
-    <>
-      <div className="bg-cyan-500 text-2xl text-white font-bold bg-center">
-        <p>Hello from chirp-frontend</p>
-      </div>
-    </>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage/>}></Route>
+        <Route path='/home' element={<HomePage/>}></Route>
+        <Route path='*' element={<ErrorPage/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
